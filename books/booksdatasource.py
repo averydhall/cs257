8e8890f001aff3d467cf33597f093b53c39962cf
elliothanson <hansone2@carleton.edu>
2021-10-02 23:50:59 -0500

    added names

--- books/booksdatasource.py
+++ books/booksdatasource.py
@@ -2,7 +2,7 @@
 '''
     booksdatasource.py
     Jeff Ondich, 21 September 2021
-
+    Edited by Elliot Hanson, and Avery Hall
     For use in the "books" assignment at the beginning of Carleton's
     CS 257 Software Design class, Fall 2021.
 '''
