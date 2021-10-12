2708ab78c5e07ac9b24e92ebe4bcc268d8a89549
elliothanson <hansone2@carleton.edu>
2021-10-11 20:23:46 -0500

    edited books.py

--- books/books.py
+++ books/books.py
@@ -13,19 +13,23 @@ def display_usage():
     f.close()
                 
 def display_titles(list_of_books_to_display):
-    for book in list_of_books_to_display:
-        authors_name_string = create_authors_string(book)
-      
-        print('"' +book.title + '"' + ", " + authors_name_string + ", " + book.publication_year)
+    if len(list_of_books_to_display) == 0:
+        print('No titles were found to include the given search string.')
+    else:
+        for book in list_of_books_to_display:
+            authors_name_string = create_authors_string(book)
+
+            print('"' +book.title + '"' + ', ' + authors_name_string + ', ' + book.publication_year)
 
 def display_authors(list_of_authors_to_display):
     for author in list_of_authors_to_display:
-        author_name = author.surname + ", " + author.given_name
-        author_life_span = " (" + author.birth_year + "-" + author.death_year + '):'
+        author_name = author.surname + ', ' + author.given_name
+        author_life_span = ' (' + author.birth_year + '-' + author.death_year + '):'
         print(author_name + author_life_span)
         
         for book in author.author_works:
             print('  "' + book.title + '" (' + book.publication_year + ')')
+        
         print()
 
 def display_range_list(list_of_books_to_display):
@@ -34,29 +38,30 @@ def display_range_list(list_of_books_to_display):
         print('"' + book.title + '", ' + authors_names_string + ', (' + book.publication_year + ')')
         
 def create_authors_string(book):
-    authors_string = ""
+    authors_string = ''
+    
     for i in range(len(book.authors)):
         author = book.authors[i]
         if i > 0 and i < len(book.authors):
-            authors_string += " and "
+            authors_string += ' and '
         authors_string += author.given_name + ' ' + author.surname
+        
     return authors_string
 
     
 def main():
     data_source = booksdatasource.BooksDataSource('books1.csv')
 
-    parser = argparse.ArgumentParser("parses arguments", add_help = False)
+    parser = argparse.ArgumentParser('parses arguments', add_help = False)
 
-    parser.add_argument('-t', '--title', type = str, default = '', dest = "title_search", nargs = "?")
-    parser.add_argument("-n", action = "store_true", dest = "sort_by_title")
-    parser.add_argument("-y", action = "store_true", dest = "sort_by_year")
-    parser.add_argument("-a", "--author", type = str, dest = "author_search", nargs = "?")
-    parser.add_argument("-r", "--range", action = "store_true", dest = "search_by_year_range")
-    parser.add_argument("-h", "-?", "--help", action = "store_true", dest = "request_help")
-    parser.add_argument("-s", "--start_yr", type = int, dest = "start_yr")
-    parser.add_argument("-e", "--end_yr", type = int, dest = "end_yr")
-
+    parser.add_argument('-t', '--title', type = str, default = '', dest = 'title_search', nargs = '?')
+    parser.add_argument('-n', action = 'store_true', dest = 'sort_by_title')
+    parser.add_argument('-y', action = 'store_true', dest = 'sort_by_year')
+    parser.add_argument('-a', '--author', type = str, default = '', dest = 'author_search', nargs = '?')
+    parser.add_argument('-r', '--range', action = 'store_true', dest = 'search_by_year_range')
+    parser.add_argument('-h', '-?', '--help', action = 'store_true', dest = 'request_help')
+    parser.add_argument('-s', '--start_yr', type = int, dest = 'start_yr')
+    parser.add_argument("-e", '--end_yr', type = int, dest = 'end_yr')
 
     args = parser.parse_args()
 
@@ -65,16 +70,15 @@ def main():
         
     elif args.title_search != '':
         if args.sort_by_year == True:
-            book_list = data_source.books(args.title_search, "year")
+            book_list = data_source.books(args.title_search, 'year')
             display_title(book_list)
         else:
-            book_list = data_source.books(args.title_search, "title")
+            book_list = data_source.books(args.title_search, 'title')
             display_titles(book_list)
 
     elif args.author_search != '':
         author_list = data_source.authors(args.author_search)
         display_authors(author_list)
-
 
     elif args.search_by_year_range:
         year_list = data_source.books_between_years(args.start_yr,args.end_yr)
